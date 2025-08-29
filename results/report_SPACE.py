0878dbde34a32770364cef2b983399f8fc98b593
Tine Colman <tinecolman@hotmail.com>
2025-08-30 00:09:58 +0200

    add openmp plots

---
+++ results/report_SPACE.py
@@ -0,0 +1,51 @@
+from io_timings import add_data
+from visualisation import *
+
+# D2.6: Progress CPU optimisation
+def load_data_cpu_progress_D26(test, cluster='meluxina', timer='total'):
+    bench_home = '/home/tcolman/Dropbox/SPACE/DATA_ARCHIVE'
+
+    data = []
+    mapping_commits = {}
+
+    # DEV Reference of public ramses version, before space (Nov 8, 2024)
+    data = add_data(data, bench_home+'/'+cluster+'/'+'benchmark_HEAD_8c72f569', test, which=timer)
+    mapping_commits['8c72f569'] = 'dev\n Nov 2024' #'starting\n reference'
+
+    # DEV Reference of public ramses version, after clean up etc (Apr 16, 2025)
+    #data = add_data(data, bench_home+'/'+cluster+'/'+'benchmark_dev_9c518f8a/'+test, which=timer)
+    #mapping_commits['9c518f8a'] = 'dev\n Apr 2025'
+
+    # nbor optims
+    data = add_data(data, bench_home+'/'+cluster+'/'+'benchmark_refactor_3cube_nbor_utils_e3a620c3', test, which=timer)
+    mapping_commits['e3a620c3'] = 'nbor\n optims'
+
+    return data, mapping_commits
+
+# D2.6: Progress OpenMP implementation
+def load_data_openmp(test, cluster='meluxina', timer='total'):
+    bench_home = '/home/tcolman/Dropbox/SPACE/DATA_ARCHIVE'
+
+    data = []
+    mapping_commits = {}
+
+    # full openMP implemenation of sedov, as mushed to ramses-romain
+    #data = add_data(data, bench_home+'/'+cluster+'/'+'benchmark_openmp_hydro_bis_a0a34a7f/'+test, which=timer)
+
+    # full openMP implemenation of multigrid for cosmo
+    data = add_data(data, bench_home+'/'+cluster+'/'+'benchmark_openmp_cosmo_0c73de54', test, which=timer)
+
+    return data, mapping_commits
+
+
+if __name__ == '__main__':
+
+    # ---- D2.6 ----
+
+    #data, mapping_commits = load_data_cpu_progress_D26('sedov','meluxina','hydro-godunov')
+    #make_table_cpu_speedup(data, reso='1024', arr_nodes=[1,2,4,8,16,32,64])
+
+    #particles, poisson-savephi, poisson-rho, poisson-phimultigrid, poisson-force
+    data, mapping_commits = load_data_openmp('cosmo','meluxina','poisson-phimultigrid')
+    make_table_openmp(data, reso='1024', arr_nodes=[1,2,4,8,16,32,64])
+    make_plot_openmp(data, reso='1024', arr_nodes=[1,2,4,8,16,32,64], outname='scaling_openmp_cosmo_phimultigrid.png')
--- results/visualisation.py
+++ results/visualisation.py
@@ -109,4 +109,73 @@ def make_table_cpu_speedup(data, reso, arr_nodes, first_column=True):
             space_report_string = '{:.3f} & {:.3f} & {:.1f} \\\\ \\hline'.format(times[0],times[-1],diff)
         print(space_report_string)
 
+# ---- OpenMP implementation ----
+
+''' Plot the strong scaling vor MPI+OpenMP versus MPI-only'''
+def make_plot_openmp(data, reso, arr_nodes, outname='scaling_openmp.png'):
+
+    labels=['MPI only', 'MPI + 2 OpenMP','MPI + 4 OpenMP','MPI + 8 OpenMP']
+
+    # create colors
+    cmap = plt.get_cmap('Greens')
+    cNorm  = colorsx.Normalize(vmin=0.25, vmax=3.75)
+    colorVals = {}
+    colorVals = [(0.1,0.1,0.7)]
+    for val in [1,2,3]:
+        colorVals.append(cmap(cNorm(val)))
+
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4.5,4.5))
+
+    ref_value = 1
+    for omp,lab,col in zip([0,2,4,8],labels,colorVals):
+        times = []
+        for nodes in arr_nodes:
+            for entry in data:
+                if entry['resolution']!=reso:
+                    continue
+                if entry['nodes']!=nodes:
+                    continue
+                if entry['omp_threads']!=omp:
+                    continue
+                # reduce time data
+                time, error_min, error_max = process_times(entry['timings'])
+                times.append(float(time))
+                if omp==0 and nodes==1:
+                    ref_value=float(time)
+        axes.plot(arr_nodes,ref_value/np.array(times),label=lab,color=col)
+
+    axes.plot(arr_nodes,arr_nodes,color='black',lw=1,ls='--')
+
+    axes.set_xscale('log')
+    axes.set_yscale('log')
+    axes.set_xlabel('nodes')
+    axes.set_ylabel('speedup')
+    axes.set_xticks([])
+    axes.set_xticks([],minor=True)
+    axes.set_xticks(arr_nodes, labels=arr_nodes)
+    axes.legend()
+    plt.savefig(outname, bbox_inches='tight', dpi=300)
+    plt.close()
+
+''' Spit out a latex table comparing different number of openMP threads '''
+def make_table_openmp(data, reso, arr_nodes):
+
+    for nodes in arr_nodes:
+        times = []
+        for omp in [0,2,4,8]:
+            for entry in data:
+                if entry['resolution']!=reso:
+                    continue
+                if entry['nodes']!=nodes:
+                    continue
+                if entry['omp_threads']!=omp:
+                    continue
+                # reduce time data
+                time, error_min, error_max = process_times(entry['timings'])
+                times.append(float(time))
+
+        # nodes MPI 2th 4th 8thr gain
+        diff = (-1)*(min(times[1],times[2],times[3]) - times[0])/times[0] * 100
+        space_report_string = '{} & {:.3f} & {:.3f} & {:.3f}& {:.3f}& {:.1f} \\\\ \\hline'.format(str(nodes).rjust(2),times[0],times[1],times[2],times[3],diff)
+        print(space_report_string)
 
