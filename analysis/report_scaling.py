86c33e9bfac0ed2a8b143527753a1202f5aa5ff3
Tine Colman <tinecolman@hotmail.com>
2026-06-02 18:02:29 +0200

    update report with weak and combo scaling

--- analysis/report_scaling.py
+++ analysis/report_scaling.py
@@ -2,11 +2,15 @@
 '''
 
 from tagged_data import *
-from resolutions import get_resolutions
+from resolutions import *
 from evolution_execution_time import plot_execution_time_multinode, table_execution_time
 from scaling_strong import plot_strong_scaling, table_strong_scaling
+from scaling_weak import plot_weak_scaling, table_weak_scaling
+from scaling_combo import plot_scaling_combo_inverse
 from openmp_config_grid import plot_mpi_omp_grid
 import argparse
+
+#---------- INPUT ------------
 
 # Get command line arguments
 parser = argparse.ArgumentParser(
@@ -19,9 +23,6 @@ args = parser.parse_args()
 # We always want to display the total time for this page
 timer='total'
 
-# Load release data
-benchmarks, release_labels = load_release_data(args.cluster, args.benchmark, timer)
-
 md = f"""# Benchmark: {args.benchmark} on {args.cluster}
 
 [Benchmark description: {args.benchmark}](../setups/{args.benchmark}/description.md)
@@ -30,40 +31,48 @@ md = f"""# Benchmark: {args.benchmark} on {args.cluster}
 
 """
 
-#resos = get_resolutions(args.benchmark)
-reso = get_resolutions(args.benchmark)[-1]
+resos = get_resolutions(args.benchmark)
 
-#for reso in reversed(resos):
+weak_scaling_map = get_weak_scaling_config2(args.benchmark)
 
-# Evolution of execution time figure
-figfile1 = f'../results/images/evo_exectime_{args.benchmark}_{reso}_{timer}_{args.cluster}.png'
-plot_execution_time_multinode(
-    benchmarks,
-    release_labels,
-    reso,
-    outname=figfile1
+######### In this part, we deal with the latest official release #########
+benchmark, release_label = load_latest_release_data(args.cluster, args.benchmark, timer)
+
+#--------- Strong and weak scaling combo figure ------------
+
+figfile = f'../results/images/scaling_combo_{args.benchmark}_{timer}_{args.cluster}.png'
+plot_scaling_combo_inverse(
+        benchmark,
+        resos,
+        weak_scaling_map,
+        outname=figfile
 )
-
-table_md1 = table_execution_time( benchmarks, release_labels, reso, fmt='markdown')
-
-# Make strong scaling figure
-figfile2 = f'../results/images/strong_scaling_{args.benchmark}_{reso}_{timer}_{args.cluster}.png'
-plot_strong_scaling(
-    benchmarks,
-    release_labels,
-    reso,
-    outname=figfile2
-)
-
-# Get strong scaling efficiency table
-table_md2 = table_strong_scaling(benchmarks, release_labels, reso, fmt='markdown')
-
 
 # Assemble markdown page string
-md += f"""
-## Evolution of execution time with code version
+md += f"""## Strong and weak scaling of the latest code release
 
-![Evolution execution time]({figfile1})
+![Scaling]({figfile})
+
+This figure shows the strong and weak scaling of the latest official release ({release_label}).
+Strong scaling can be inferred diagonally, while weak scaling is determined by reading horizontally.
+Dotted lines show ideal scaling.
+
+"""
+
+######### In this part, we deal with the time evolution between versions #########
+benchmarks, release_labels = load_release_data(args.cluster, args.benchmark, timer)
+reso = resos[-1] # we show only the highest resolution
+
+#--------- Execution time evolution ------------
+
+figfile = f'../results/images/evo_exectime_{args.benchmark}_{reso}_{timer}_{args.cluster}.png'
+plot_execution_time_multinode(benchmarks, release_labels, reso, outname=figfile)
+
+table_md = table_execution_time(benchmarks, release_labels, reso, fmt='markdown')
+
+md += f"""## Evolution of execution time with code version
+
+![Evolution execution time]({figfile})
 
 This figure shows the execution time for different versions of the code.
 To guide the eye, the dotted line shows the time for the oldest version.
@@ -72,11 +81,21 @@ The last column informs of the speedup of the last version with
 respect to the first listed version. Unless otherwise stated,
 the time listed is for runs with MPI-only using the full compute node.
 
-{table_md1}
+{table_md}
 
-## Strong scaling
+"""
 
-![Strong scaling]({figfile2})
+
+#--------- Strong scaling evolution ------------
+
+figfile = f'../results/images/strong_scaling_{args.benchmark}_{reso}_{timer}_{args.cluster}.png'
+plot_strong_scaling(benchmarks, release_labels, reso, outname=figfile)
+
+table_md = table_strong_scaling(benchmarks, release_labels, reso, fmt='markdown')
+
+md += f"""## Strong scaling evolution
+
+![Strong scaling]({figfile})
 
 This figure shows the strong scaling for different versions of the code.
 The underlying timings are those from the previous section.
@@ -86,12 +105,34 @@ with respect to the minimal number of nodes.
 Unless otherwise stated, data is for runs with MPI-only 
 using the full compute node.
 
-{table_md2}
+{table_md}
 
 """
 
+#--------- Weak scaling evolution ------------
 
-# Weak scaling
+nodes, resos = get_weak_scaling_config(args.benchmark)
+
+figfile = f'../results/images/weak_scaling_{args.benchmark}_{timer}_{args.cluster}.png'
+plot_weak_scaling(benchmarks, release_labels, nodes, resos, outname=figfile)
+
+table_md = table_weak_scaling(benchmarks, release_labels, nodes, resos, fmt='markdown')
+
+
+md += f"""## Weak scaling evolution
+
+![Weak scaling]({figfile})
+
+This figure shows the weak scaling for different versions of the code.
+Ideal scaling is shown as a dotted line.
+The table lists the corresponding weak scaling efficiency,
+with respect to the minimal number of nodes.
+Unless otherwise stated, data is for runs with MPI-only 
+using the full compute node.
+
+{table_md}
+
+"""
 
 
 
--- results/results_sedov_meluxina.md
+++ results/results_sedov_meluxina.md
@@ -4,6 +4,13 @@
 
 [Cluster info: meluxina](../HPCclusters/meluxina/cluster_description.md)
 
+## Strong and weak scaling of the latest code release
+
+![Scaling](../results/images/scaling_combo_sedov_total_meluxina.png)
+
+This figure shows the strong and weak scaling of the latest official release (2026-05).
+Strong scaling can be inferred diagonally, while weak scaling is determined by reading horizontally.
+Dotted lines show ideal scaling.
 
 ## Evolution of execution time with code version
 
@@ -27,7 +34,7 @@ the time listed is for runs with MPI-only using the full compute node.
 | 64 | 3.19 | 3.22 | 2.97 | 2.84 | 2.82 (MPI=64 OMP=2) | 11.7 |
 
 
-## Strong scaling
+## Strong scaling evolution
 
 ![Strong scaling](../results/images/strong_scaling_sedov_1024_total_meluxina.png)
 
@@ -48,5 +55,23 @@ using the full compute node.
 | 16 | 0.922 | 0.919 | 0.906 | 0.904 | 0.922 (MPI=64 OMP=2) |
 | 32 | 0.902 | 0.886 | 0.882 | 0.875 | 0.888 |
 | 64 | 0.822 | 0.805 | 0.776 | 0.737 | 0.739 (MPI=64 OMP=2) |
+
+
+## Weak scaling evolution
+
+![Weak scaling](../results/images/weak_scaling_sedov_total_meluxina.png)
+
+This figure shows the weak scaling for different versions of the code.
+Ideal scaling is shown as a dotted line.
+The table lists the corresponding weak scaling efficiency,
+with respect to the minimal number of nodes.
+Unless otherwise stated, data is for runs with MPI-only 
+using the full compute node.
+
+| Nodes | Resolution | 2024-10 | 2025-05 | 2025-10 | 2026-05 | openmp |
+|---|---|---|---|---|---|---|
+| 1 | 256 | 1.000 (MPI=128 OMP=0) | - | - | 1.000 (MPI=128 OMP=0) | - |
+| 8 | 512 | 0.903 (MPI=128 OMP=0) | - | - | 0.950 (MPI=128 OMP=0) | - |
+| 64 | 1024 | 0.941 (MPI=128 OMP=0) | 1.000 (MPI=128 OMP=0) | 1.000 (MPI=128 OMP=0) | 0.849 (MPI=128 OMP=0) | 1.000 (MPI=64 OMP=2) |
 
 
