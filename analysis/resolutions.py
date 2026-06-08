bd65b0fef1fbcf749eb5f7c58dbfd2c7b3f765b4
Tine Colman <tinecolman@hotmail.com>
2026-06-08 14:37:41 +0200

    update resolutions

--- analysis/resolutions.py
+++ analysis/resolutions.py
@@ -2,16 +2,21 @@
 def get_resolutions(test):
     if test=='sedov':
         return ['256','512','1024']
+    elif test=='sedov-amr':
+        return ['lvl5-8','lvl5-10']
     else:
-        print('Add resolutions for benchmark:', test)
+        print('ERROR: Add resolutions for benchmark:', test)
         exit(1)
 
 def get_weak_scaling_config(test):
     if test=='sedov':
         nodes = [1,8,64]
         resos = get_resolutions(test)
+    elif test=='sedov-amr':
+        nodes = [1,2]
+        resos = get_resolutions(test)
     else:
-        print('Add weak scaling configuration for benchmark:', test)
+        print('ERROR: Add weak scaling configuration for benchmark:', test)
         exit(1)
     return nodes, resos
 
@@ -21,7 +26,11 @@ def get_weak_scaling_config2(test):
             '256': 1,
             '512': 8,
             '1024': 64}
+    elif test=='sedov-amr':
+        weak_scaling_map = {
+            'lvl5-8': 1,
+            'lvl5-10': 2}
     else:
-        print('Add weak scaling configuration for benchmark:', test)
+        print('ERROR: Add weak scaling configuration for benchmark:', test)
         exit(1)
     return weak_scaling_map
